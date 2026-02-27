c6a71d8e98e0e6fff5a63153aafb7bf186c074d6
Your Name <you@example.com>
2026-02-27 22:16:14 +0300

    add comments

--- src/requests/RequestsBuilder.ts
+++ src/requests/RequestsBuilder.ts
@@ -9,48 +9,62 @@ type TaskRequest = {
 }
 
 const MAX_PUSH_INTERVAL_MS = 100;
-const MAX_PUSH_SIZE = 100;
+const MAX_PUSH_SIZE = 500;
 
 export class RequestsBuilder {
     tasks: TaskRequest[] = []
-    completedTasks: TaskRequest[] = []
     lastPushTime = Date.now()
-    debugProfilerTime = Date.now()
     ticker: NodeJS.Timeout | undefined = undefined
+    counter: number = 0; // это для уникальных id
 
+    /*
+    Для чего нужен таймер:
+     По сути здесь реализован примитивный поллинг для запросов.
+     Каждую мс, когда в списке есть неотправленные запросы вызывается метод processTasks,
+     который проверяет, можно ли отправить пакет запросов (прошло ли достаточно времени - MAX_PUSH_INTERVAL_MS с
+     последнего, или если набралось MAX_PUSH_SIZE).
+     Если все запросы сделаны (список тасок пуст), то поллинг останавливается.
+     */
     startTimer() {
-        if (!this.ticker){
+        if (!this.ticker) {
             this.ticker = setInterval(() => {
-                // console.log("tick", this.getTime())
                 this.processTasks().then()
             }, 1);
         }
     }
 
     stopTimer() {
-        if(this.ticker){
+        if (this.ticker) {
             clearInterval(this.ticker)
             this.ticker = undefined
         }
     }
 
 
+    /*
+     Временная функция для ассинхронной задержки
+     */
     private sleep(ms: number) {
         return new Promise<void>(resolve => setTimeout(resolve, ms));
     }
 
+
+    /*
+    Добавление таски в список неотправленных + обновление времени последней добавленной таски.
+     */
     async addTaskRequest(task: TaskRequest) {
         this.tasks.push(task)
         this.lastPushTime = Date.now()
     }
 
-
+    /*
+    Основной метод для отправки тасок.
+     */
     async processTasks() {
         if (this.tasks.length === 0) {
             this.stopTimer()
             return
         }
-        // console.log("process tasks", this.getTime())
         if (this.tasks.length >= 5 || Date.now() - this.lastPushTime > MAX_PUSH_INTERVAL_MS) {
             let counter = 0
             const part = []
@@ -62,46 +76,51 @@ export class RequestsBuilder {
                 part.push(task)
                 counter++
             }
-            await this.makeRequests(part)
+            await this.sendPacket(part)
         }
     }
 
-    async makeRequests(part: TaskRequest[]) {
-        const query = part.map(task => task.requested_id)
-        // console.log("execute query with ids", query, this.getTime())
-        await this.sleep(50)
+
+    /*
+    Отправка пакета данных.
+     */
+    async sendPacket(part: TaskRequest[]) {
+        const query = part.map(task => task.requested_id) // список id, которые мы отправляем
+        await this.sleep(50) // имитация запроса к api.
         for (const task of part) {
             task.result = "complete"
             task.status = "DONE"
-            task.resolve("result")
+            task.resolve("result value from api") // разблокируем поток в основном промисе. Отправляем а него результат таски
         }
-        // console.log(`part with ids: ${query} done`, this.getTime())
     }
 
-
+    /*
+    Основной внешний метод. Он создает запросы.
+     */
     async makeRequest(id: number) {
-        this.startTimer()
-        // console.log(`add request with id ${id}`, this.getTime());
-        let resolveRequest!: (value: unknown | PromiseLike<unknown>) => void;
+        this.startTimer() // запускаем таймер, если он еще не запущен
+        let resolveRequest!: (value: unknown | PromiseLike<unknown>) => void; // внешний резольвер для промиса, чтобы иметь возможность разблокировать промис извне
         const waitForResult = new Promise(resolve => {
             resolveRequest = resolve;
         });
 
         await this.addTaskRequest(
             {
-                id: id,
+                id: ++this.counter,
                 requested_id: id,
                 status: "WAITING",
                 result: undefined,
                 resolve: resolveRequest
             }
-        )
-        await waitForResult
-        return `ok ${id}`
+        ) // создаем объект с таской
+        const result = await waitForResult // ожидаем разблокировки извне
+        return `ok ${id} ${result}` // возвращаем результат
     }
 }
 
-// Один инстанс на один request (на один рендер страницы)
+/*
+cache гарантирует один объект билдера на весь рендер. По сути синглтон, но через next.
+ */
 export const getRequestsBuilder = cache(() => {
     return new RequestsBuilder();
 });
